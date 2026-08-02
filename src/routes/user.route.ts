ad9b5cdcbeaa407d333cd01f922cf5e22400a70b
Gyananjay Nayak <nayakgyananjay1999@gmail.com>
2026-08-02 22:51:19 +0530

    feat: added user module with crud api

---
+++ src/routes/user.route.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { UserController } from "../controllers/user.controller";
+
+const router = express.Router();
+
+router.get("/", UserController.getUserList);
+router.post("/", UserController.createUser);
+router.get("/:id", UserController.getuserById);
+router.put("/:id", UserController.updateUserById);
+router.delete("/:id", UserController.deleteUserById);
+
+export default router;
