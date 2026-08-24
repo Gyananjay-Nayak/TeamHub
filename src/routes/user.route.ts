9fd6ad0b78ff0fde51c5947843313bdaedb644b9
Gyananjay Nayak <nayakgyananjay1999@gmail.com>
2026-08-24 15:50:53 +0530

    refactor: add request validation middleware

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { UserController } from "../controllers/user.controller";
 
+import {
+  validateCreateUser,
+  validateUpdateUser,
+} from "../middlewares/validation";
+
 const router = express.Router();
 
 router.get("/", UserController.getUserList);
-router.post("/", UserController.createUser);
+router.post("/", validateCreateUser, UserController.createUser);
 router.get("/:id", UserController.getUserById);
-router.put("/:id", UserController.updateUserById);
+router.put("/:id", validateUpdateUser, UserController.updateUserById);
 router.delete("/:id", UserController.deleteUserById);
 
 export default router;
